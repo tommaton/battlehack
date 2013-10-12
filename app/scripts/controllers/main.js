3c918def9de636b0cddd364dce91b14440081352
Owain Llewellyn <owain.llew@gmail.com>
2013-10-12 21:49:35 +0100

    fixed persistance

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,9 @@
 
 window.APP.controller('MainCtrl', ['$scope', 'globalServices', '$location', function ($scope, globalServices, $location) {
     function init() {
-        console.log('main ctrller called', 'route is: /');
         if($scope.$parent.isUserLoggedIn()){
             $location.path('home');
         } else {
-            console.log('redirect user to login');
             $location.path('login');
         }
 
