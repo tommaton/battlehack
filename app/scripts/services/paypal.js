3d4c3371e84e7eef1c6340d55ee451a10a3fa870
Owain Llewellyn <owain.llew@gmail.com>
2013-10-12 17:14:09 +0100

    ...

--- app/scripts/services/paypal.js
+++ app/scripts/services/paypal.js
@@ -1,5 +1,19 @@
 'use strict';
 
-window.APP.service('Paypal', function Paypal() {
+window.APP.service('Paypal', ['$http', '$q', function Paypal($http, $q) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+
+    this.createPayment = function() {
+        var deferred = $q.defer();
+
+        $http.post('paypal', price, description)
+            .success(function(data) {
+                return deferred.resolve(data);
+            })
+            .error(function(data) {
+                return deferred.reject(data);
+            });
+        
+        return deferred.promise;
+    }
 });
