b960594db990d193bb54ab8c7a04a87ebf332bb7
Owain Llewellyn <owain.llew@gmail.com>
2013-10-13 01:54:28 +0100

    ...

--- app/scripts/controllers/contactUser.js
+++ app/scripts/controllers/contactUser.js
@@ -10,16 +10,16 @@ window.APP.controller('ContactuserCtrl', ['$scope', '$routeParams', '$location',
             
             $scope.neighbour = $routeParams.userId;
             $scope.productId = $routeParams.productId;
-
+            $scope.msg = null;
             $scope.getProduct($scope.productId);
         }
 
         $scope.count = "0";
 
-        var commentEl = angular.element('#comment');
+        var commentEl = angular.element('#msg');
 
         commentEl.on('keyup', function(e) {
-            $scope.count = (158 - parseInt(this.value.length));
+            $scope.count = (118 - parseInt(this.value.length));
         })
     }
 
@@ -31,9 +31,9 @@ window.APP.controller('ContactuserCtrl', ['$scope', '$routeParams', '$location',
         });
     };
 
-    $scope.sendTxt = function() {
+    $scope.sendTxt = function(number) {
         $scope.$emit('LOADING');
-        Twillio.sendTxt('447837043238', 'Neighbour.ly: The User, ' + $scope.product.user.name + ' has requested the use of your ' + $scope.product.title + '. Visit: http://localhost:8000/#/home').then(function(response) {
+        Twillio.sendTxt('447837043238', 'Neighbour.ly: ' + $scope.product.user.name + ' has a question about your ' + $scope.product.title + '.' + $scope.msg).then(function(response) {
             $scope.$emit('NOTLOADING');
             $scope.response = response;
 
