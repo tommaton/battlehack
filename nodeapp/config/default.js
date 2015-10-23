5982fa7d3320582d052db66a6bcfec2a885d3c6b
Francis Gilbert <francis.gilbert@akqa.com>
2015-10-23 15:23:16 +0100

    Removing settings for private tokens.

--- nodeapp/config/default.js
+++ nodeapp/config/default.js
@@ -1,15 +1,15 @@
 module.exports = {
     paypal: {
-        clientId: "AdeqvhCTW_h3FI3Zv96BQUNvWnaJfuicpddS5WyqSLYrZElghhiwQRXsGFYt",
-        clientSecret:  "ECuYzxCLb7vhh0ozh3xHtm-iy_XMKP8iSvjky5fe1vhNl1tExelpm7QxeyiU",
+        clientId: "",
+        clientSecret:  "",
         returnUrl: "http:\/\/localhost:8080\/#\/offerSuccess?productid=%s&username=%s",
         cancelUrl: "http:\/\/localhost:8080\/#\/paymentcancel"
     },
 
     twilio: {
-        sid: "AC3c3b4bbff8b2371a5ca86250ae981f4f",
-        authToken: "9f87261dc3292ba8895af6c1a35d9abe",
-        fromNumber: "441325952405"
+        sid: "",
+        authToken: "",
+        fromNumber: ""
     },
 
     justgiving: {
